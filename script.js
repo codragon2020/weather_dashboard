b1e7dacedcfe6f6c568438bb623ee4b68e7a1d4b
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-22 15:34:24 -0500

    Adding variables for the getElementById requests

---
+++ script.js
@@ -0,0 +1,11 @@
+var inputEl = document.getElementById("city-input");
+var searchEl = document.getElementById("search-button");
+var clearEl = document.getElementById("clear-history");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");4
+var currentWindEl = document.getElementById("wind-speed");
+var currentUVEl = document.getElementById("UV-index");
+
+var APIKey = "98e7da6ba47e6e46bbb4c11d566fa749"
