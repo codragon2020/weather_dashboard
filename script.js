0373ae12299e4f7cde2ac6b983d9c00f332da883
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-24 08:12:10 -0500

    Added 5 day forecast and stylying

--- script.js
+++ script.js
@@ -90,5 +90,26 @@ searchButton.click(function() {
             });
 
         })
+
+        $.ajax({
+            url: urlFiveDay,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            // Array for 5-days 
+            var day = [0, 8, 16, 24, 32];
+            // var fiveDayCard = $(".forecast").addClass("card-body");
+            var fiveDayDiv = $(".forecast").addClass("card-text");
+            fiveDayDiv.empty();
+
+            // For each for 5 days
+            day.forEach(function (i) {
+                var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
+
+                fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+            })
+        })
+    
     }
 })
